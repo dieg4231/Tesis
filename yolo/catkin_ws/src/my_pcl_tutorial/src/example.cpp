ebe1fbc37b68ba2f67755e592962ae620f15e1dd
dieg4231 <dieg4231@gmail.com>
2020-07-27 23:22:09 -0500

    se reconocen los aruco y se obtiene distancia Pose (X,y,z) y anfle respecto a la camara

---
+++ yolo/catkin_ws/src/my_pcl_tutorial/src/example.cpp
@@ -0,0 +1,214 @@
+#include <ros/ros.h>
+// PCL specific includes
+#include <sensor_msgs/PointCloud2.h>
+#include <std_msgs/UInt32MultiArray.h>
+#include <std_msgs/UInt32.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+
+#include <pcl/surface/convex_hull.h>
+#include <pcl/segmentation/extract_polygonal_prism_data.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/common/centroid.h>
+
+#include <string>
+#include <my_pcl_tutorial/Landmarks.h>
+
+ros::Publisher pub;
+pcl::PointIndices::Ptr indx(new pcl::PointIndices ());;
+std::string device;
+
+std::vector<unsigned int> separators;
+std::vector<unsigned int> ids;
+
+void 
+cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+{
+//std::cout << "atixxxxxx \n";
+  pcl::PointIndices::Ptr indx2(new pcl::PointIndices ());
+  std::vector<unsigned int> separators2;
+std::vector<unsigned int> ids2;
+
+  *indx2 = *indx;
+  separators2 = separators;
+  ids2 = ids;
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_pcl(new pcl::PointCloud<pcl::PointXYZ>),
+                                      cloud_pcl_filtered(new pcl::PointCloud<pcl::PointXYZ>), 
+                                      cloud_plane_polygon (new pcl::PointCloud<pcl::PointXYZ>),
+                                      cloud_roi (new pcl::PointCloud<pcl::PointXYZ>);
+  
+  pcl::fromROSMsg(*cloud_msg,*cloud_pcl);
+/*  
+  // Perform the actual filtering
+  pcl::VoxelGrid<pcl::PointXYZ> sor;
+  sor.setInputCloud (cloud_pcl);
+  sor.setLeafSize (0.1, 0.1, 0.1);
+  sor.filter (*cloud_pcl_filtered);
+
+  cloud_plane_polygon->push_back (pcl::PointXYZ (0.0,1, 1));
+  cloud_plane_polygon->push_back (pcl::PointXYZ (0.0,-1, 1));
+  cloud_plane_polygon->push_back (pcl::PointXYZ (0.0,-1, -1));
+  cloud_plane_polygon->push_back (pcl::PointXYZ (0.0,1, -1)); 
+
+  
+  pcl::PointCloud<pcl::PointXYZ>::Ptr hull_points (new pcl::PointCloud<pcl::PointXYZ>) ;
+  pcl::ConvexHull<pcl::PointXYZ> hull;
+
+  // hull.setDimension (2); // not necessarily needed, but we need to check the dimensionality of the output
+  hull.setInputCloud(cloud_plane_polygon);
+  hull.reconstruct (*hull_points);
+
+  pcl::PointIndices::Ptr indices (new pcl::PointIndices ());
+
+  
+
+  if (hull.getDimension () == 2)
+  {
+    pcl::ExtractPolygonalPrismData<pcl::PointXYZ> prism;
+    prism.setInputCloud (cloud_pcl_filtered);
+    prism.setInputPlanarHull (hull_points);
+    prism.setHeightLimits (0, 10);
+    prism.segment (*indices);
+  }
+  else
+    PCL_ERROR ("The input cloud does not represent a planar surface.\n");
+ */
+/*
+  pcl::PointIndices::Ptr indx(new pcl::PointIndices ());
+  int cta=0;
+  for(int y = 374; y < 419;y++ )
+    for(int x=690; x < 737; x++,cta++ )   
+      indx->indices.push_back(1280*y+x);
+
+/*
+  indx->indices.clear();
+  for(int i=0; i<153600;i++)
+    indx->indices.push_back(i);
+*/
+  
+
+  pcl::ExtractIndices<pcl::PointXYZ> filter;
+  filter.setInputCloud (cloud_pcl);//_filtered);
+  filter.setIndices (indx2);//ices);
+  // Extract the points in cloud_in referenced by indices_in as a separate point cloud:
+  filter.setKeepOrganized (true);
+  filter.filter (*cloud_roi);
+
+  // Convert to ROS data type
+  sensor_msgs::PointCloud2 output;
+  
+  pcl::toROSMsg(*cloud_roi,output);
+  
+  std_msgs::Header header;
+ // if(device.compare("kinect") == 0 )
+  //{
+    header.frame_id = "camera_depth_optical_frame"; //Kinect
+   // std::cout << "atooo \n";
+  //}else if(device.compare("zed_stereo") == 0)
+  //{
+   // header.frame_id = "base_link"; // ZED
+   // std::cout << "ati \n";
+ // }
+ // else
+  //{
+    //std::cout << "You mustn't be here" << device << std::endl;
+  //}
+  
+  output.header = header;
+  // Publish the data
+  pub.publish (output);
+
+  
+
+  //for(std::vector<unsigned int>::const_iterator it_ids = ids2.begin(); it_ids != ids2.end(); ++it_ids)
+  for(int i = 0; i < ids2.size() ; i++)
+  {
+    pcl::CentroidPoint<pcl::PointXYZ> centroid;
+    for( int j =  i == 0 ? 0 : separators2[i-1]; j < separators2[i] ; j++)//   cloud_roi->points.begin() ; it < cloud_roi->points.end(); it++)
+    {
+      //std::cout << "sii22" << indx2->indices[j] << std::endl;
+      if( !isnan(cloud_roi->points[indx2->indices[j]].x) && !isnan(cloud_roi->points[indx2->indices[j]].y) && !isnan(cloud_roi->points[indx2->indices[j]].z)  )
+      {
+        centroid.add (pcl::PointXYZ ( cloud_roi->points[indx2->indices[j]].x, cloud_roi->points[indx2->indices[j]].y, cloud_roi->points[indx2->indices[j]].z) );
+        //std::cout << "sii" << cloud_roi->points[indx2->indices[j]].x<< std::endl;
+      }
+      //std::cout << it->x << ", " << it->y << ", " << it->z << std::endl;
+    } 
+
+    pcl::PointXYZ c1;
+    centroid.get (c1);
+
+    std::cout << "ARUCO: " << ids2[i] << " X: " << c1.x << ", Y: " << c1.y << ", Z: " << c1.z << " DIST: " <<  sqrt( pow(c1.x,2)+pow(c1.y,2)+pow(c1.z,2) ) << " Ang: " << atan2(c1.z,c1.x)-(M_PI/2) << std::endl;
+  
+  }
+
+}
+
+
+void 
+indicesCallback (const my_pcl_tutorial::LandmarksPtr& indices)
+{
+  //std::cout << "atiwwwwwwwww \n";
+  indx->indices.clear();
+  separators.clear();
+  ids.clear();
+
+  for(std::vector<unsigned int>::const_iterator it = indices->masks_index.begin(); it != indices->masks_index.end(); ++it)
+    indx->indices.push_back(*it);
+  
+  for(std::vector<unsigned int>::const_iterator it = indices->separators.begin(); it != indices->separators.end(); ++it)
+    separators.push_back(*it);
+  
+  for(std::vector<unsigned int>::const_iterator it = indices->ids.begin(); it != indices->ids.end(); ++it)
+    ids.push_back(*it);
+
+}
+
+
+
+int
+main (int argc, char** argv)
+{
+  // Initialize ROS
+  ros::init (argc, argv, "my_pcl_tutorial");
+  ros::NodeHandle nh;
+
+  if(ros::param::has("~device"))
+  {
+    ros::param::get("~device", device);
+    std::cout << "Device: " << device << std::endl;
+  }
+  else
+  {
+    std::cout << "Sorry device not found. Try: kinect/zed_stereo" << device << std::endl;
+    return 0;
+  }
+
+
+  // Create a ROS subscriber for the input point cloud
+  //if(device.compare("kinect") == 0)
+    
+    ros::Subscriber sub_cloud = nh.subscribe ("/camera/depth_registered/points", 1, cloud_cb);
+    //ros::Subscriber sub_cloud = nh.subscribe ("/zed/zed_node/point_cloud/cloud_registered", 1, cloud_cb);
+  /*else if(device.compare("zed_stereo") == 0)
+  {
+    std::cout << "aquuuoio \n";
+    ros::Subscriber sub_cloud = nh.subscribe ("/zed/zed_node/point_cloud/cloud_registered", 1, cloud_cb);
+  }
+  else
+  {
+    std::cout << "Sorry U_U" << device << std::endl;
+    return 0;
+  }*/
+  
+  ros::Subscriber sub_indices = nh.subscribe ("/indices", 1, indicesCallback);
+
+  // Create a ROS publisher for the output point cloud
+  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+
+  // Spin
+  ros::spin ();
+}
---
+++ yolo/catkin_ws/src/my_pcl_tutorial/src/get_qr.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+import rospy
+import sys
+import cv2
+#import cv2.cv as cv
+from my_pcl_tutorial.msg import Landmarks
+from sensor_msgs.msg import Image
+from std_msgs.msg import UInt32MultiArray, Header
+from cv_bridge import CvBridge
+import numpy as np
+
+class cvBridgeDemo():
+    def __init__(self):
+        self.node_name = "cv_bridge_demo"
+
+        rospy.init_node(self.node_name)
+
+        # What we do during shutdown
+        rospy.on_shutdown(self.cleanup)
+
+        
+        if rospy.has_param('~device') == True:
+            device = rospy.get_param('~device')
+            if not(device == 'kinect' or device == 'zed_stereo'):
+                print("Sorry try. kinect/zed_stereo")
+                sys.exit()
+
+        else:
+            print("Sorry device not found. Try kinect/zed_stereo")
+            sys.exit()
+        # Create the OpenCV display window for the RGB image
+        #self.cv_window_name = self.node_name
+        #cv.NamedWindow(self.cv_window_name, cv.CV_WINDOW_NORMAL)
+        #cv.MoveWindow(self.cv_window_name, 25, 75)
+
+        # Create the cv_bridge object
+        self.bridge = CvBridge()
+
+        # Subscribe to the camera image and depth topics and set
+        # the appropriate callbacks
+        
+        if device == 'zed_stereo':
+            self.image_sub = rospy.Subscriber("/zed/zed_node/left/image_rect_color", Image, self.image_callback)
+        elif device == 'kinect':
+            self.image_sub = rospy.Subscriber("/camera/rgb/image_rect_color", Image, self.image_callback)
+        
+        self.pub_indices = rospy.Publisher('/indices', Landmarks, queue_size=1)
+
+    def image_callback(self, ros_image):
+        # Use cv_bridge() to convert the ROS image to OpenCV format
+        frame = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
+  
+
+        # Convert the image to a Numpy array since most cv2 functions
+        # require Numpy arrays.
+        imageCopy = np.array(frame, dtype=np.uint8)
+        # Get dimensions of image
+        image_width = imageCopy.shape[1]
+        #print(image_width)
+        image_height = imageCopy.shape[0]
+
+        dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
+
+        # Initialize the detector parameters using default values
+        parameters =  cv2.aruco.DetectorParameters_create()
+
+        # Detect the markers in the image
+        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(imageCopy, dictionary)
+        
+        idex_of_codes = []
+        array_aux = []
+        separators = []
+        ids = []
+        if np.all(markerIds is not None):
+            
+            
+           
+            for i in range(0, len(markerIds)):
+                #rvec, tvec = cv.aruco.estimatePoseSingleMarkers(markerCorners[i], 0.16, cameraMatrix, distCoeffs)
+                #print("Rotation")
+                #print(rvec)
+                #print("Distancia :")
+                #print(  math.sqrt( pow(tvec[0][0][0],2) + pow(tvec[0][0][1],2) + pow(tvec[0][0][2],2) )    )
+                #print("Traslacion:")
+                #print(tvec)
+                imageCopy = cv2.aruco.drawDetectedMarkers(imageCopy, markerCorners, markerIds)
+                #imageCopy = cv2.aruco.drawAxis(imageCopy, cameraMatrix, distCoeffs, rvec, tvec, 0.01)  # Draw Axis
+                min_x = markerCorners[i][0][0][0] 
+                min_y = markerCorners[i][0][0][1] 
+
+                max_x = markerCorners[i][0][2][0] 
+                max_y = markerCorners[i][0][2][1]
+                
+                reducttion = (max_x-min_x)/4
+
+                min_x += int(reducttion)
+                max_x -= int(reducttion)
+
+                min_y += int(reducttion)
+                max_y -= int(reducttion)
+
+                array_aux = np.arange( (max_y-min_y+1 )*(max_x-min_x+1 ) )
+
+                j = 0
+                for y in range(int(min_y),int(max_y)):
+                    for x in range(int(min_x),int(max_x)):
+                        array_aux[j] = image_width*y+(x)
+                        j = j+1
+                if i==0:
+                    separators.append(j)
+                else:
+                    separators.append(separators[-1]+j)
+
+                idex_of_codes=np.append (idex_of_codes, array_aux)
+                ids.append(markerIds[i][0])
+
+        landmarks_pub = Landmarks() 
+
+        landmarks_pub.ids = ids
+        landmarks_pub.separators = separators
+        landmarks_pub.masks_index = idex_of_codes
+        
+        
+        self.pub_indices.publish(landmarks_pub)
+        cv2.imshow("out",imageCopy)
+
+
+
+        # Process any keyboard commands
+        self.keystroke = cv2.waitKey(5)
+        if 32 <= self.keystroke and self.keystroke < 128:
+            cc = chr(self.keystroke).lower()
+            if cc == 'q':
+                # The user has press the q key, so exit
+                rospy.signal_shutdown("User hit q key to quit.")
+
+    def cleanup(self):
+        print "Shutting down vision node."
+        cv2.destroyAllWindows()   
+
+def main(args):       
+    try:
+        cvBridgeDemo()
+        rospy.spin()
+    except KeyboardInterrupt:
+        print "Shutting down vision node."
+        cv.DestroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
